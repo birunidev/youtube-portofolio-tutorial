9fb5772589677d49da0e7634662f38c2eb1232e8
birunidev <birunimamak@gmail.com>
2020-04-07 22:09:28 +0700

    adding showcase

--- js/script.js
+++ js/script.js
@@ -11,3 +11,44 @@ backdrop.addEventListener("click", () => {
   sidedrawer.classList.remove("active");
   backdrop.classList.remove("show");
 });
+
+// showcase part
+const showcaseImageContainer = document.getElementById("showcase-image");
+const indicators = document.querySelectorAll(".indicator");
+console.log(indicators);
+// <img src="assets/portfolio1.png" alt="" />
+let images = [1, 2, 3, 4];
+
+window.onload = () => {
+  let i = 1;
+  renderImage(1);
+  // setInterval(() => {
+  //   if (i > 4) {
+  //     i = 1;
+  //   }
+  //   renderImage(i);
+  //   i++;
+  // }, 2000);
+};
+
+for (const indicator of indicators) {
+  indicator.addEventListener("click", (e) => {
+    renderImage(e.target.id);
+  });
+}
+
+function renderImage(imageNum) {
+  let imageHTMl = ``;
+
+  for (const indicator of indicators) {
+    if (imageNum === indicator.id) {
+      indicator.classList.add("active");
+    } else {
+      indicator.classList.remove("active");
+    }
+  }
+
+  showcaseImageContainer.innerHTML = `<img src="assets/portfolio${imageNum}.png" alt="" />`;
+}
+
+// showcase part
